70331636624cf569af21fe6392ee230057c27cb2
Gambino-stack <69907371+Gambino-stack@users.noreply.github.com>
2020-11-25 08:25:55 +0100

    add a show method and view

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/program", name="program_")
+ * Class ProgramController
+ * @package App\Controller
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/index/", name="index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", methods={"GET"}, name="show", requirements={"id"="^[0-9]*[1-9][0-9]*$"})
+     * @param int $id
+     * @return Response
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id
         ]);
     }
 }
