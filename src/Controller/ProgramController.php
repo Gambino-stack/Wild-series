e175c82f5e02286936599a96cdb84f6d488f59cf
Gambino-stack <69907371+Gambino-stack@users.noreply.github.com>
2020-11-30 13:16:34 +0100

    Quete 8 OK

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,35 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 /**
- * @Route("/program", name="program_")
  * Class ProgramController
  * @package App\Controller
+ * @Route ("/Programs", name="program_")
  */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/index", name="index")
+     * @Route("/", name="index")
+     * @return Response
      */
     public function index(): Response
     {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
+            'programs' => $programs
         ]);
     }
 
     /**
-     * @Route("/{id}", methods={"GET"}, name="show", requirements={"id"="\d+"})
+     * @Route("/show/{id<^[0-9]+$>}", name="show")
      * @param int $id
      * @return Response
      */
     public function show(int $id): Response
     {
-        return $this->render('program/show.html.twig', ['id' => $id
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if(!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+
+        return $this->render('program/show.html.twig', ['program' => $program
         ]);
     }
 }
