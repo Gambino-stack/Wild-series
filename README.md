934dfd4c32f76c44f35bfbd028135f20ded09e7f
Gambino-stack <69907371+Gambino-stack@users.noreply.github.com>
2020-12-07 11:52:56 +0100

    Quete 13 ok

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Lien pour la video de la quête 11 : https://www.loom.com/share/762ca898cd6d4124
 Liens pour les vidéos de la  Quête 12: 
 https://www.loom.com/share/762ca898cd6d412489450b5ff6179dd8
 https://www.loom.com/share/a06c3990078248b289ba380e3398fec5
+Liens pour la video de la Quête 13 : https://www.loom.com/share/a848fd18b8434e3dbff038159b74a71a
--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,9 +6,13 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @UniqueEntity("title", message="ce titre existe déjà")
  */
 class Program
 {
@@ -27,12 +31,15 @@ class Program
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Length(max="255")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
      */
     private $summary;
 
